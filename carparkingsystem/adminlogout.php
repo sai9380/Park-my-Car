f7487c625dd0c0bbd25ae595ac36ce0a07aa43cc
Saiprasad Patil <patilsai4321@gmail.com>
2026-07-06 11:24:30 +0530

    Remove Unwanted folder

--- carparkingsystem/adminlogout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_destroy();
-?>
-<script>
-    alert ("Logged out successfully");
-    window.location.href = "adminindex.html";
-</script>
--- carparkingsystem/sessioncheck.php
+++
@@ -1,5 +0,0 @@
-<?php
-if(isset($_SESSION['log']) == "") {
-    header("location:index.php");
-}
-?>
